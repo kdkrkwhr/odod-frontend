5088de1dc1f61bbf6fb245bfbca6c1b054bdc863
vortrefflich <memo.osten@gmail.com>
2021-06-06 21:50:24 +0900

    add image Callout

--- src/components/map/Marker.tsx
+++ src/components/map/Marker.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
-import { Marker as MarkerElement } from 'react-native-maps';
+import { Marker as MarkerElement, Callout } from 'react-native-maps';
 import { StyleSheet, View, Text, Image } from 'react-native';
 import markerImage from '../../assets/images/marker.png';
+import roadImage0 from '../../assets/images/road/00.jpeg';
+import roadImage1 from '../../assets/images/road/01.jpeg';
+import roadImage2 from '../../assets/images/road/02.jpeg';
+import roadImage3 from '../../assets/images/road/03.jpeg';
+import roadImage4 from '../../assets/images/road/04.jpeg';
+import CalloutArrow from './CalloutArrow';
+
+const roadImageList = [
+  roadImage0, roadImage1, roadImage2, roadImage3, roadImage4
+];
 
 interface ICoordinate {
   latitude: number;
@@ -12,12 +22,13 @@ interface IProps {
     coordinate: ICoordinate;
     title: string;
     description: string;
-  }
+  };
+  index: number;
 };
 
 const round2 = (target: number) => Math.round(target * 100) / 100;
 
-const Marker = ({ mark }: IProps) => {
+const Marker = ({ mark, index }: IProps) => {
   const onSelect = (event) => {
     console.log('marker selected');
   }
@@ -25,11 +36,42 @@ const Marker = ({ mark }: IProps) => {
     <MarkerElement
       coordinate={mark.coordinate}
       image={markerImage}
-      title={mark.title}
-      description={mark.description}
       onSelect={onSelect}
     >
+      <Callout tooltip>
+        <View>
+          <View style={styles.bubble}>
+            <Text style={styles.name}>{'Image no.' + mark.title}</Text>
+            <Image
+              style={styles.image}
+              source={roadImageList[index%5]}
+            />
+          </View>
+          <CalloutArrow />
+        </View>
+      </Callout>
     </MarkerElement>
   );
 };
 export default Marker;
+
+const styles = StyleSheet.create({
+  bubble: {
+    flexDirection: 'column',
+    alignSelf: 'flex-start',
+    backgroundColor: '#fff',
+    borderRadius: 6,
+    borderColor: '#ccc',
+    borderWidth: 0.5,
+    padding: 15,
+    width: 150,
+  },
+  name: {
+    fontSize: 16,
+    marginBottom: 5,
+  },
+  image: {
+    width: 120,
+    height: 80
+  },
+});
