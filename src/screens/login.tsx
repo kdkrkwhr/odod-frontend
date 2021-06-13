de9e147b6ac2fd3f0da82d397d0ad2601c240a52
DEUK CHEON, OH <guruwang2@gmail.com>
2021-06-13 09:29:20 +0900

    로그인 API 연동 완료

--- src/screens/login.tsx
+++ src/screens/login.tsx
@@ -4,9 +4,10 @@ import Header from "../components/header/Header";
 import { moderateScale, verticalScale, scale } from "../components/util/Screen";
 import * as Google from "expo-google-app-auth";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { useNavigation } from "@react-navigation/native";
+import { useFocusEffect, useNavigation } from "@react-navigation/native";
 import { useDispatch } from "react-redux";
 import { updateUserInfo } from "../reducer/UserInfoReducer";
+import { authenticate, register } from "../service/auth/AuthService";
 
 type Props = {};
 
@@ -18,12 +19,27 @@ const Login = (props: Props) => {
     loginCheck();
   }, []);
 
+  // useFocusEffect(
+  //   React.useCallback(() => {
+  //     // Do something when the screen is focused
+  //     loginCheck();
+  //     return () => {
+  //       // Do something when the screen is unfocused
+  //     };
+  //   }, [])
+  // );
+
   const loginCheck = async () => {
     const token = await AsyncStorage.getItem("token");
-    alert("token값 :" + token);
-    if (token) {
-      navigation.navigate("Home");
-    }
+    const email = await AsyncStorage.getItem("email");
+    if (!token) return;
+    const res = await authenticate(token, email);
+    const isUser = res.data.userCheck;
+    if (!isUser) return;
+
+    dispatch(updateUserInfo({ token: token }));
+    alert("자동 로그인 완료");
+    navigation.navigate("Home");
   };
 
   const googleLogin = async () => {
@@ -36,10 +52,21 @@ const Login = (props: Props) => {
     });
 
     if (result.type === "success") {
-      const token = result.user.id;
-      await AsyncStorage.setItem("token", result.user.id || "");
       //TODO : accessToken 받는 API 호출
+      const data = {
+        email: result.user.email,
+        name: result.user.name,
+        picture: result.user.photoUrl,
+      };
+      const res = await register(data);
+      const token = res.data?.token ?? "";
+      if (!token) {
+        alert("로그인 실패");
+        return;
+      }
       dispatch(updateUserInfo({ token: token }));
+      await AsyncStorage.setItem("token", token || "");
+      await AsyncStorage.setItem("email", data.email || "");
       navigation.navigate("Home");
     }
   };
